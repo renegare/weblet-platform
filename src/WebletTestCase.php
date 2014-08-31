d6df9ae97b9c7b1e53559cfb4704927a4e7a3c53
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-08-31 21:40:38 +0100

    add WebletTestCase for platform weblet

---
+++ src/WebletTestCase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Renegare\Weblet\Platform;
+
+use Renegare\Weblet\Base\WebletTestCase as BaseWebletTestCase;
+use Renegare\Weblet\Base\Weblet as BaseWeblet;
+use Renegare\Soauth\Credentials;
+use Renegare\Soauth\SoauthTestCaseTrait;
+
+class WebletTestCase extends BaseWebletTestCase {
+    use SoauthTestCaseTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createApplication() {
+        $app = new Weblet(['debug' => true]);
+        set_exception_handler(null);
+        return $app;
+    }
+
+    public function configureApplication(BaseWeblet $app) {
+        $app['debug'] = true;
+        $app['exception_handler']->disable();
+    }
+
+    public function createAuthenticatedClient(array $credentialAttrs = [], $createdTime = null, array $server = [], WebApplication $app = null) {
+        $app = $app? $app : $this->getApplication();
+
+        $credentials = $this->createCredentials($credentialAttrs);
+        $this->saveCredentials($credentials, $createdTime, $app);
+
+        $server = array_merge(['HTTP_X_ACCESS_CODE' => $credentials->getAccessCode()], $server);
+        $client = $this->createClient($server, $app);
+        return $client;
+    }
+}
--- test/phpunit/WebletTestCase.php
+++ test/phpunit/WebletTestCase.php
@@ -2,41 +2,7 @@
 
 namespace Renegare\Weblet\Platform\Test;
 
-use Renegare\Weblet\Base\WebletTestCase as WTC;
-use Renegare\Weblet\Base\Weblet as BaseWeblet;
-use Renegare\Weblet\Platform\Weblet as PlatformWeblet;
-use Renegare\Soauth\Credentials;
-use Renegare\Soauth\SoauthTestCaseTrait;
+use Renegare\Weblet\Platform\WebletTestCase as PlatformWebletTestCase;
 
-class WebletTestCase extends WTC {
-    use SoauthTestCaseTrait;
-
-    public function getService($name) {
-        return $this->getApplication()[$name];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function createApplication() {
-        $app = new PlatformWeblet(['debug' => true]);
-        set_exception_handler(null);
-        return $app;
-    }
-
-    public function configureApplication(BaseWeblet $app) {
-        $app['debug'] = true;
-        $app['exception_handler']->disable();
-    }
-
-    public function createAuthenticatedClient(array $credentialAttrs = [], $createdTime = null, array $server = [], WebApplication $app = null) {
-        $app = $app? $app : $this->getApplication();
-
-        $credentials = $this->createCredentials($credentialAttrs);
-        $this->saveCredentials($credentials, $createdTime, $app);
-
-        $server = array_merge(['HTTP_X_ACCESS_CODE' => $credentials->getAccessCode()], $server);
-        $client = $this->createClient($server, $app);
-        return $client;
-    }
+class WebletTestCase extends PlatformWebletTestCase {
 }
